bcbae0c171c58aee9f847bde4afc61fd438d3a31
Jason Adams <jayadams23@gmail.com>
2023-01-16 12:56:49 -0800

    sales,contact,CSS,JS,README,index

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+let person = {
+  name: 'Jason',
+  age: 36,
+  class: '201d96',
+  likeCodeFellows: true,
+  speak: function () {
+    console.log('Code Fellows is great');
+  },
+};
+
+console.log(person);
+instructor.speak();
+
+
+
+
+
+let Seattle = {
+
+};
+
+let Tokyo = {
+
+};
+
+let Dubai = {
+
+};
+
+let Paris = {
+
+};
+
+let Lima = {
+
+};
